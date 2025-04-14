59ae87097a0c6291d45b1cdc63d7bfc97b9c02e0
yse-yoo <youhei.yoshikawa@yse-c.net>
2025-04-14 15:33:34 +0900

    update

--- crud/update_user.php
+++ crud/update_user.php
@@ -5,9 +5,11 @@ require_once '../lib/Database.php';
 // POSTチェック
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // TODO: idを取得
-    $id = 0;
+    $id = $_POST['id'];
     // TODO: passwordを取得
-    $password = "";
+    $password = $_POST['password'];
+    // var_dump($id, $password);
+    // exit;
     $result = update($id, $password);
 }
 
