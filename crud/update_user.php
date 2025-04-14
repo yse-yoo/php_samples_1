23cbf69471152ad12dc502ed8a03e02550b23f77
yse-yoo <youhei.yoshikawa@yse-c.net>
2025-04-14 16:10:28 +0900

    update

--- crud/update_user.php
+++ crud/update_user.php
@@ -18,12 +18,12 @@ function update($id, $password)
 {
     try {
         // TODO: パスワードハッシュ化
-        $hash = "";
+        $hash = password_hash($password, PASSWORD_DEFAULT);
         // DB接続
         $pdo = Database::getInstance();
         // TODO: ユーザ情報を更新するSQLを作成
         // プレスホルダー（:id） 
-        $sql = "";
+        $sql = "UPDATE users SET password = :password WHERE id = :id";
         // SQL事前準備
         $stmt = $pdo->prepare($sql);
         // プレスホルダー（:id） のパラメータを引数にSQL実行
