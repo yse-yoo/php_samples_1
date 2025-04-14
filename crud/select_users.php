15e4fc3ae9da41be8cd891f051cd870ff5b9bd4b
yse-yoo <youhei.yoshikawa@yse-c.net>
2025-04-14 14:09:21 +0900

    update

--- crud/select_users.php
+++ crud/select_users.php
@@ -12,7 +12,7 @@ function get($limit = 50)
         // TODO: DB接続
         $pdo = Database::getInstance();
         // TODO: SQL作成
-        $sql = "SELECT * FROM users LIMIT $limit";
+        $sql = "SELECT * FROM users ORDER BY id DESC LIMIT $limit";
         // TODO: Userデータを取得
         $stmt = $pdo->query($sql);
         // TODO: Userデータを取得
